4f741dccc12749ac939045fd5e119a9e3bb6372b
Arian Derida <IA9268@PID-PROJECT-IA9268.local>
2024-06-21 13:52:15 +0700

    section 6: nest common validation rule

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class createTaskDto { 
+
+  @IsString()
+  @IsNotEmpty()
   task_name: string;
   task_description: string;
 }
--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,6 +1,10 @@
+import { IsOptional } from "class-validator";
+
 export class updateTaskDto { 
+
   task_name: string;
   task_description: string;
+  @IsOptional()
   created_at: Date;
   updated_at?: Date;
   deleted_at?: Date;
